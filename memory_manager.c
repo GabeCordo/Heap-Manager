206e480c0f45dcf58e82fb6c559e4ea4d3e1158e
Gabriel Cordovado <gabe.cordovado@yahoo.ca>
2020-06-27 19:15:23 -0400

    added vm page managment for dynamic mem, added merging/spliting meta-blocks

--- memory_manager.c
+++ memory_manager.c
@@ -35,7 +35,6 @@ static void *mm_get_new_vm_page_from_kernal(int units) {
 		return NULL; //if the kernal request fails, we will return null
 	memset(vm_page, 0, units * SYSTEM_PAGE_SIZE);
 	return (void *)vm_page;
-	
 }
 
 /*
@@ -53,7 +52,6 @@ static void *mm_return_vm_page_to_kernel(void *vm_page, int units) {
 	//munmap will return a boolean, so if it fails the condition will be triggered
 	if(munmap(vm_page, units * SYSTEM_PAGE_SIZE))
 		return NULL;
-
 }
 
 /*
@@ -147,6 +145,20 @@ void mm_print_registered_page_families() {
 	}
 }
 
+/*
+ * Function:  lookup_page_family_by_name
+ * -------------------------------------
+ * iterates over every page for families starting with
+ * the one most-recently created by the application
+ * untill the page_family is matched by name
+ *
+ *  *struct_name: an array of characters representing 
+ *				  the page-family name we arte trying
+ *				  to find
+ *
+ *	returns: the pointer to the page-family that matches
+ *			 the name provided as an argument
+ */
 vm_page_family_t *lookup_page_family_by_name(char *struct_name) {
 	
 	vm_page_for_family_t *vm_page_family_curr = NULL;
@@ -166,4 +178,141 @@ vm_page_family_t *lookup_page_family_by_name(char *struct_name) {
 	}
 	
 	return NULL;
+}
+
+/*
+ * Function:  mm_union_free_blocks
+ * -------------------------------
+ * merge two meta-blocks and there data-blocks together
+ * in order to form one meta-block/data-block that if
+ * the two are CONSECUTIVLY FREE
+ *
+ *  *first: the meta-block belonging to a lower memory
+ *			address on the vm page for families
+ *  *second: the meta-block belonging to a higher memory
+ * 			 address on the vm page for families
+ *
+ *	returns: nothing
+ */
+static void mm_union_free_blocks(block_meta_data_t *first, block_meta_data_t *second) {
+	
+	//we only want to perform a merge if both values are currently free
+	assert(first->is_free == MM_TRUE && second->is_free == MM_TRUE);
+	
+	first->block_size += sizeof(block_meta_data_t) + second->block_size;
+	first->next_block = second->next_block;
+	
+	//only update the prev pointer of the ante-penultimate if it is NOT NULL
+	if (second->next_block)
+		second->next_block->prev_block = first;
+}
+
+/*
+ * Function:  mm_is_vm_page_empty
+ * -------------------------------
+ * merge two meta-blocks and there data-blocks together
+ * in order to form one meta-block/data-block that if
+ * the two are CONSECUTIVLY FREE
+ *
+ *  *vm_page: a pointer to the vm data page (NOT FAMILY)
+ *
+ *	returns: MM_TRUE if the vm page given as a function
+ *			 argument is complelty free else it returns
+ * 			 MM_FALSE (type vm_bool_t)
+ */
+vm_bool_t *mm_is_vm_page_empty(vm_page_t *vm_page) {
+	
+	if (vm_page_t_ptr->_next_block == NULL &&
+		vm_page_t_ptr->_prev_block == NULL &&
+		vm_page_t_ptr->is_free == MM_TRUE) {
+		
+		return MM_TRUE;
+	}
+	return MM_FALSE;
+}
+
+/*
+ * Function:  mm_max_page_allocatable_memory
+ * -----------------------------------------
+ * determines the maximum allocatable memory (bytes)
+ * available to n vm-pages, taking into acount the bytes
+ * needed to store the meta-data of the page-memory
+ *
+ *  units: an integer indicating the number of vm pages
+ *
+ *	returns: a uint32 representing the number of bytes that
+ *			 can be allocated by another process
+ */
+static inline uint32_t mm_max_page_allocatable_memory(int units) {
+	
+	return (uint32_t)((SYSTEM_PAGE_SIZE * units) - offset_of(vm_page_t, page_memory));
+}
+
+/*
+ * Function:  allocate_vm_page
+ * ---------------------------
+ * allocates a new vm page for data-storage. It will be appended to
+ * the front of the linked list connecting all data vm-pages
+ *
+ *  *vm_page_family: the pointer to the vm_page_family the vm data
+ *					 pointer will belong to
+ *
+ *	returns: a newely allocated virtual page with a configured
+ *			 page-memory segment for pf_family, next, prev pointers
+ */
+vm_page_t * allocate_vm_page(vm_page_family_t *vm_page_family) {
+	
+	vm_page_t *vm_page = mm_get_new_vm_page_from_kernal(1); //get a new vm-page from the kernal
+	MARK_VM_PAGE_EMPTY(vm_page); //initialize the vm-page to have a meta-block attached
+	
+	vm_page->block_meta_data.block_size = mm_max_page_allocatable_memory(1);
+	vm_page->block_meta_data.offset = offset_of(vm_page_t, block_meta_data);
+	
+	vm_page->next = NULL;
+	vm_page->prev = NULL;
+	vm_page->pg_family = vm_page_family; //the pointer must direct to the vm_page_family argument
+	
+	//check to see if the new vm data page is the first in the vm-page-family
+	if(!vm_page_family->first_page) {
+		vm_page_family->first_page = vm_page;
+		return vm_page;
+	}
+	
+	//place the new vm data page at the head of the ll
+	vm_page->next = vm_page_family->first_page; //point to the old first-vmpage of the ll
+	vm_page_family->first_page = vm_page;
+	return vm_page;
+}
+
+/*
+ * Function:  mm_vm_page_delete_and_free
+ * -------------------------------------
+ * delete's the vm_page passed as an argument from the linked list
+ * of vm data pages belonging to a page_family
+ *
+ *  *vm_page: the pointer to the vm data page that was connected to
+ * 			  the vm_family_page
+ *
+ *	returns: nothing
+ */
+void mm_vm_page_delete_and_free(vm_page_t *vm_page) {
+	
+	vm_page_family_t *vm_page_family = vm_page->pg_family;
+	
+	//check to see if the vm-page is at the front of the linked list
+	if(vm_page_family->first_page == vm_page) {
+		vm_page_family->first_page = vm_page->next;
+		if(vm_page->next)
+			vm_page->next->prev = NULL;
+		vm_page->next = NULL;
+		vm_page->prev = NULL;
+		mm_return_vm_page_to_kernel((void *)vm_page, 1);
+		return;
+	}
+	
+	//check to see if the vm-page is in the middle of the linked list
+	if(vm_page->next) //check to see if we get a null value (if we do, it's the end of the ll)
+		vm_page->next->prev = vm_page->prev;
+	vm_page->prev->next = vm_page->next;
+	mm_return_vm_page_to_kernel((void *)vm_page, 1);
 }
--- memory_manager.h
+++ memory_manager.h
@@ -14,7 +14,7 @@
 		
 		char struct_name [MM_MAX_STRUCT_NAME];
 		uint32_t struct_size;
-			
+	
 	} vm_page_family_t;
 
 	typedef struct vm_page_families_ {
@@ -33,6 +33,16 @@
 		uint32_t offset;
 		
 	} block_meta_data_t;
+	
+	typedef struct vm_page_ {
+		
+		struct vm_page_ *next;
+		struct vm_page_ *prev;
+		struct vm_page_family_ *pg_family; //points back to the the page_family
+		block_meta_data_t block_meta_data; //the first meta-block on the vm page
+		char page_memeory[0]; //first data-block in the vm page
+		
+	} vm_page_t;
 		
 	/* macro declarations */
 
@@ -65,10 +75,33 @@
 	#define PREV_META_BLOCK(block_meta_data_ptr)	\
 		(block_meta_data_ptr->_prev_block)
 		
+	#define mm_bind_blocks_for_allocation(allocated_meta_block, free_meta_block) \
+		free_meta_block->prev_block = allocated_meta_block;		\
+		free_meta_block->next_block = allocated_meta_block->next_block;		\
+		allocated_meta_block->next_block = free_meta_block;		\
+		if (free_meta_nlock->next_block)		\
+			free_meta_block->next_block->prev_block = free_meta_block
+			
+	#define MARK_VM_PAGE_EMPTY(vm_page_t_ptr)				\
+		//sets a pre-existing vm page to empty (most-likely so it can be freed)
+		vm_page_t_ptr->block_meta_data.next_block = NULL;	\
+		vm_page_t_ptr->block_meta_data.prev_block = NULL;	\
+		vm_page_t_ptr->block_meta_data.is_free = MM_TRUE
+		
+	#define ITERATE_VM_PAGE_BEGIN(vm_page_family_ptr, curr)	\
+		for (curr = ; ; curr ) {
+	
+	#define ITERATE_VM_PAGE_END(vm_page_family_ptr, curr) }}
+		
 	/* function declarations */
 
 	static void *mm_get_new_vm_page_from_kernal(int units);
 	static void *mm_return_vm_page_to_kernel(void *vm_page, int units);
 	vm_page_family_t *lookup_page_family_by_name(char *struct_name);
+	static void mm_union_free_blocks(block_meta_data_t *first, block_meta_data_t *second);
+	vm_bool_t *mm_is_vm_page_empty(vm_page_t *vm_page);
+	static inline uint32_t mm_max_page_allocatable_memory(int units);
+	vm_page_t * allocate_vm_page(vm_page_family_t *vm_page_family);
+	void mm_vm_page_delete_and_free(vm_page_t *vm_page);
 
 #endif
